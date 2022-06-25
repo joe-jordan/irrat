bbd3bfc33f8b4386ec06aa307ee857c4b92b1422
Joe Jordan <joe@joe-jordan.co.uk>
2022-06-25 20:11:24 +0100

    first example file, showing that largish prime factors (>23) are simplified in some cases.

---
+++ examples/exact_cancellation.py
@@ -0,0 +1,25 @@
+from irrat import *
+
+a = rat(5, 7)
+b = rat(5476, 1053)
+
+c = a / b
+# c = (5 * 1053) / (7 * 5476)
+print(str(c))
+# 0.1373526035688198
+print(repr(c))
+# rat(5265, 38332)
+
+# the repr should always be simplified (deliberately use a prime above 23, which means it won't
+# be auto-simplified.)
+
+d = rat(47*53, 3*3*23)
+e = rat(3*19, 53*73)
+f = d * e
+
+print(str(f))
+# 0.17728806829461982
+print(repr(f))
+# rat(893, 5037)
+
+# TODO use evaluate(precision=100) to generate a long string.
